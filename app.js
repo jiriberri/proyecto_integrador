5d25607bfb596595da062fd4b868d2883947291d
jiriberri <julianiriberri@gmail.com>
2021-09-22 16:47:53 -0300

    agregue procfile y actualice el app para heroku

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 const publicFolderPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicFolderPath));
 
-app.listen(8000, () => {
-  console.log("Servidor corriendo en el puerto 8000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Servidor corriendo en el puerto 3000");
 });
 
 app.get("/", (req, res) => {
